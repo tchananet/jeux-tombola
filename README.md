98e6ea31d607037f7d61f0769b2bc002d85c71b2
unknown <netacy2015@gmail.com>
2025-03-08 13:23:32 +0100

    First init modified only the questions

---
+++ README.md
@@ -0,0 +1,55 @@
+
+# Who Wants to Be a Millionaire - Trivia Quiz
+
+<img src="https://github.com/Signor1/trivia_game/blob/4f08c1f18ca9d76f9d2ae78466cee8f308cbd731/Trivia-Game1.png" width="100%" height="400"/>
+Welcome to the Who Wants to Be a Millionaire - Trivia Quiz! This is a React.js app that replicates the popular game show "Who Wants to Be a Millionaire". You can play the game and test your knowledge in different categories such as history, science, music, and more!
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Demo](#demo)
+- [Technologies Used](#technologies-used)
+- [Contributing](#contributing)
+
+## Installation
+
+To install and run the app locally, please follow these steps:
+
+1. Clone this repository to your local machine.
+2. Open the terminal and navigate to the project directory.
+3. Run `npm install` to install the necessary dependencies.
+4. Run `npm start` to start the app on your local server.
+5. Open your browser and navigate to `http://localhost:3000`.
+
+## Usage
+
+Once the app is running, you can start by entering your username and clicking on the "Start Game" button on the home page to start playing the game. You will be presented with a series of multiple-choice questions with four possible answers. You have to select the correct answer within a limited amount of time to advance to the next question.
+
+If you answer all the questions correctly, you will become a millionaire!
+
+## Demo
+
+You can try the app live on [live Demo](https://signorquiz.vercel.app/).
+
+<img src="https://github.com/Signor1/trivia_game/blob/b5d56e9727262d53ed285cf138fdf450ee7f790e/Trivia-Game2.png" width="100%" height="400"/>
+<img src="https://github.com/Signor1/trivia_game/blob/7a20bc9c80645a6454f71793dbaaf81acaf472f3/Trivia-Game3.png" width="100%" height="400"/>
+
+## Technologies Used
+
+This app was built with the following technologies:
+
+- React.js
+- HTML/CSS
+- JavaScript
+
+## Contributing
+
+If you would like to contribute to the project, please follow these steps:
+
+1. Fork this repository.
+2. Create a new branch with your changes: `git checkout -b my-feature-branch`.
+3. Make changes and commit them: `git commit -m "my feature description"`.
+4. Push to the branch: `git push origin my-feature-branch`.
+5. Submit a pull request.
+
---
+++ src/data/questions.js
@@ -0,0 +1,124 @@
+export const ListOfQuestions = [
+  {
+    id: 1,
+    question: "Pourquoi la Journée Internationale de la Femme est-elle célébrée chaque année le 8 mars ?",
+    answers: [
+      { text: "Pour célébrer les réalisations des femmes et sensibiliser aux inégalités de genre", correct: true },
+      { text: "Pour fêter les femmes de la royauté", correct: false },
+      { text: "Pour organiser des défilés et des festivités", correct: false },
+      { text: "Pour marquer la fin de l’hiver", correct: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "Qui a été la première femme à recevoir le prix Nobel de la Paix ?",
+    answers: [
+      { text: "Marie Curie", correct: false },
+      { text: "Malala Yousafzai", correct: false },
+      { text: "Mother Teresa", correct: true },
+      { text: "Wangari Maathai", correct: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "En quelle année les Nations Unies ont-elles proclamé la Journée Internationale de la Femme ?",
+    answers: [
+      { text: "1950", correct: false },
+      { text: "1977", correct: true },
+      { text: "1990", correct: false },
+      { text: "2000", correct: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "Quelle est la première femme à diriger un pays africain ?",
+    answers: [
+      { text: "Ngozi Okonjo-Iweala", correct: false },
+      { text: "Ellen Johnson Sirleaf", correct: true },
+      { text: "Winnie Mandela", correct: false },
+      { text: "Miriam Makeba", correct: false },
+    ],
+  },
+  {
+    id: 5,
+    question: "Quel type de produits Alpha Motors distribue-t-elle au Cameroun ?",
+    answers: [
+      { text: "Produits électroniques", correct: false },
+      { text: "Voitures et véhicules", correct: true },
+      { text: "Vêtements et accessoires", correct: false },
+      { text: "Téléphones mobiles", correct: false },
+    ],
+  },
+  {
+    id: 6,
+    question: "Quel est un des services majeurs offerts par Alpha Motors ?",
+    answers: [
+      { text: "Réparation de téléphones", correct: false },
+      { text: "Vente de véhicules et services après-vente", correct: true },
+      { text: "Construction d’infrastructures", correct: false },
+      { text: "Vente de carburant", correct: false },
+    ],
+  },
+  {
+    id: 7,
+    question: "Alpha Motors représente quelle marque de voiture ?",
+    answers: [
+      { text: "Toyota", correct: false },
+      { text: "Peugeot", correct: false },
+      { text: "Mercedes-Benz", correct: false },
+      { text: "(Mettre ici la marque spécifique qu’ils représentent)", correct: true },
+    ],
+  },
+  {
+    id: 8,
+    question: "Quelle marque automobile a un logo qui représente une femme, en référence à l’esprit de liberté et d'indépendance ?",
+    answers: [
+      { text: "BMW", correct: false },
+      { text: "Mercedes-Benz (anciennement)", correct: true },
+      { text: "Audi", correct: false },
+      { text: "Renault", correct: false },
+    ],
+  },
+  {
+    id: 9,
+    question: "Quel modèle de voiture est souvent considéré comme étant particulièrement populaire auprès des femmes en raison de son design élégant ?",
+    answers: [
+      { text: "Fiat 500", correct: true },
+      { text: "Lamborghini Aventador", correct: false },
+      { text: "Tesla Model X", correct: false },
+      { text: "Land Rover Defender", correct: false },
+    ],
+  },
+  {
+    id: 10,
+    question: "Quel pays a été le premier à accorder le droit de vote aux femmes ?",
+    answers: [
+      { text: "France", correct: false },
+      { text: "États-Unis", correct: false },
+      { text: "Nouvelle-Zélande", correct: true },
+      { text: "Royaume-Uni", correct: false },
+    ],
+  },
+  {
+    id: 11,
+    question: "Quelle femme est connue pour son combat pour l’éducation des filles et a reçu le prix Nobel de la Paix en 2014 ?",
+    answers: [
+      { text: "Angela Merkel", correct: false },
+      { text: "Oprah Winfrey", correct: false },
+      { text: "Malala Yousafzai", correct: true },
+      { text: "Serena Williams", correct: false },
+    ],
+  },
+  {
+    id: 12,
+    question: "Quelle voiture est surnommée la 'voiture du peuple' ?",
+    answers: [
+      { text: "Ferrari", correct: false },
+      { text: "Volkswagen Beetle", correct: true },
+      { text: "Tesla Model S", correct: false },
+      { text: "Rolls-Royce Phantom", correct: false },
+    ],
+  }
+];
+
+// export default ListOfQuestions;
